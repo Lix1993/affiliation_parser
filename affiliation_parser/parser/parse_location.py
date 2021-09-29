2a70b0ac8560800efdec54130ea49d57a7b85855
Li Xing <lix930701@gmail.com>
2021-09-29 10:43:55 +0800

    update country recognize method

--- affiliation_parser/parser/parse_location.py
+++ affiliation_parser/parser/parse_location.py
@@ -3,14 +3,37 @@ import re
 from ..data import REGION_ALIAS
 
 
+def get_joined_elements(words, number):
+    result = []
+
+    for i in range(len(words) - number + 1):
+        result.append(' '.join(words[i:i + number]))
+
+    return result
+
+
 def find_country(location: str):
     """
-    Find country from string
+    Find country from string list
     """
-    location_lower = location.lower()
-    for alias in REGION_ALIAS:
-        if alias.lower() in location_lower:
-            return REGION_ALIAS[alias]
+    location_lower = location.lower().strip(' .')
+    splits = re.split(r'\.\s|,|;|\(|\)', location_lower)
+    splits = [i.strip().lower() for i in splits if i]
+
+    # single word match country name or alias
+    for country in REGION_ALIAS:
+        if set(REGION_ALIAS[country]).intersection(set(splits)):
+            return country
+
+    # two words or more to match
+    double_elems = get_joined_elements(splits[-4:], 2)
+    triple_elems = get_joined_elements(splits[-4:], 3)
+    quadruple_elems = get_joined_elements(splits[-4:], 4)
+    word_tuple = double_elems + triple_elems + quadruple_elems
+    for country in REGION_ALIAS:
+        if set(REGION_ALIAS[country]).intersection(set(word_tuple)):
+            return country
+
     return ""
 
 
@@ -18,7 +41,7 @@ def parse_location(location):
     """
     Parse location and country from affiliation string
     """
-    location = re.sub(r"\.", "", location).strip()
+
     country = find_country(location)
     dict_location = {"location": location.strip(), "country": country.strip()}
     return dict_location
